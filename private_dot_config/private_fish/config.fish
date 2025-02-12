026e2b9e6ef3490a09c239bf34a14418b156bacb
sgg-jhodgson <james.hodgson@superglitchgames.com>
2025-02-12 09:02:19 +0000

    add fish config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    function fish_greeting
+        neofetch
+    end
+
+    set -gx EDITOR helix
+end
+
+# Created by `pipx` on 2025-01-31 07:18:01
+set PATH $PATH ~/.local/bin
